7ee2b84388afb6fe925bea76977d33dd60701053
Sarah Lopez <slopez323@gmail.com>
2022-04-13 08:49:34 -0700

    challenge 2

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+if (localStorage.getItem('count')){
+    let count = localStorage.getItem('count');
+    localStorage.setItem('count', +count + 1);    
+} else localStorage.setItem('count',1);
+
 $('form').on('submit', function(){
     localStorage.setItem('string', $('#stringInput').val());
 });
 
 if (localStorage.getItem('string')){
-    $('#stringDisplay').text(localStorage.getItem('string'));
+    $('#stringDisplay').text(`Text: ${localStorage.getItem('string')}`);
+};
+
+if (localStorage.getItem('count')){
+    $('#showCount').text(`Refresh count: ${localStorage.getItem('count')}`);
 };
