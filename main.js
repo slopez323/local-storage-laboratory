8f719cde5c7bfe3888c79fa31049691fbc86da7b
Sarah Lopez <slopez323@gmail.com>
2022-04-13 11:59:26 -0700

    challenge 3

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-if (localStorage.getItem('count')){
-    let count = localStorage.getItem('count');
-    localStorage.setItem('count', +count + 1);    
-} else localStorage.setItem('count',1);
+let arr = JSON.parse(localStorage.getItem('list')) || [];
+displayList();
 
-$('form').on('submit', function(){
+if (localStorage.getItem('count')) {
+    let count = localStorage.getItem('count');
+    localStorage.setItem('count', +count + 1);
+} else localStorage.setItem('count', 1);
+
+$('#stringChallenge').on('submit', function () {
     localStorage.setItem('string', $('#stringInput').val());
 });
 
-if (localStorage.getItem('string')){
+if (localStorage.getItem('string')) {
     $('#stringDisplay').text(`Text: ${localStorage.getItem('string')}`);
 };
 
-if (localStorage.getItem('count')){
+if (localStorage.getItem('count')) {
     $('#showCount').text(`Refresh count: ${localStorage.getItem('count')}`);
+};
+
+$('#listBuilder').on('submit', function (evnt) {
+    arr.push($('#listInput').val());
+    localStorage.setItem('list', JSON.stringify(arr));
+    displayList();
+});
+
+function displayList() {
+    $('ol').empty();
+    for(let i of arr){
+        // console.log(arr[i])
+        $('ol').append(`<li>${i}</li>`);
+    };
 };
